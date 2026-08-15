45c41b53ea0ebe3ef5d61461851f99b61fee9a89
lionelalma-coder <alienzalma@gmail.com>
2026-08-15 13:51:55 +0300

    Create sitemap[.]xml.ts

---
+++ src/routes/sitemap[.]xml.ts
@@ -0,0 +1,25 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/sitemap[.]xml')({
+  server: {
+    handlers: {
+      GET: async () => {
+        // Replace or add your website's clean pages below inside the <urlset> block
+        const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://sitemaps.org">
+  <url>
+    <loc>https://nothingbuthouse.events</loc>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+        return new Response(xml, {
+          headers: {
+            'Content-Type': 'application/xml; charset=utf-8',
+            'X-Content-Type-Options': 'nosniff',
+          },
+        });
+      },
+    },
+  },
+});
